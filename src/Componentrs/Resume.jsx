b1a109d016fe995551c0b1538a94fe06a39ce595
mit <meetrakholiya31@gmail.com>
2025-11-05 10:26:02 +0530

    update

--- src/Componentrs/Resume.jsx
+++ src/Componentrs/Resume.jsx
@@ -17,7 +17,7 @@ const Resume = () => {
                     <div className="flex justify-between pb-[50px] sm:px-[0px] px-[20px] ">
                         <h3 className='text-[26px] font-bold'>Experience</h3>
                         <button className='btn1'><a
-                            href="Resume.pdf"
+                            href="/Resume.pdf"
                             download="Mit Rakholiya Resume.pdf"> DOWNLOAD CV</a>
                             </button>
                     </div>
