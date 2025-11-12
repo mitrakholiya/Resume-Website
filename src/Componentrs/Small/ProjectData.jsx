2562486f469d17d2cb811615c3cfe063351a534c
mit <meetrakholiya31@gmail.com>
2025-11-12 19:39:05 +0530

    Update

--- src/Componentrs/Small/ProjectData.jsx
+++ src/Componentrs/Small/ProjectData.jsx
@@ -4,48 +4,58 @@ import img3 from '../../assets/Web template/3.png'
 import img4 from '../../assets/Web template/4.png'
 import img5 from '../../assets/Web template/5.png'
 import img6 from '../../assets/Web template/6.png'
+import img7 from '../../assets/Web template/7.png'
 
 export let webTemp = [
+
+
   {
-    giturl :"https://github.com/mitrakholiya/internet-Research",
-    name: "Internet Research",
+    giturl: "#",
+    name: "Portfolio Website",
     role: "Frontend Developer",
-    para: "Designed a static responsive webpage using HTML and CSS to display research content with a clean and minimal layout.",
-    url: img1
+    para: "Developed a Portfolio Website using React, Bootstrap, HTML, and CSS. Focused on component-based structure, responsive UI, and product layouts.",
+    url: img7
   },
   {
-    giturl :"#",
-    name: "Amazon Clone",
-    role: "Frontend Developer",
-    para: "Built a clone of Amazon’s landing page using only HTML and CSS, focusing on layout, navigation bar, and responsive product sections.",
-    url: img2
-  },
-  {
-    giturl :"https://github.com/mitrakholiya/Ninom-Fruit-Template",
-    name: "Fruit Shop",
-    role: "Frontend Developer",
-    para: "Created an e-commerce template for a fruit shop using HTML, CSS, and Bootstrap. Implemented grid layouts and responsive components for better UX.",
-    url: img3
-  },
-  {
-    giturl :"https://github.com/mitrakholiya/Coffee-Website",
-    name: "Coffee Shop",
-    role: "Frontend Developer",
-    para: "Developed a stylish coffee shop website using HTML, CSS, and Bootstrap. Focused on responsive design and modern UI components.",
-    url: img4
-  },
-  {
-    giturl :"https://github.com/mitrakholiya/Ice-Cream-Web-site",
+    giturl: "https://github.com/mitrakholiya/Ice-Cream-Web-site",
     name: "Ice-Cream Store",
     role: "Frontend Developer",
     para: "Built a fun and engaging ice-cream shop template using HTML, CSS, Bootstrap, and JavaScript. Added interactive elements for better user engagement.",
     url: img5
   },
   {
-    giturl :"#",
+    giturl: "https://github.com/mitrakholiya/Coffee-Website",
+    name: "Coffee Shop",
+    role: "Frontend Developer",
+    para: "Developed a stylish coffee shop website using HTML, CSS, and Bootstrap. Focused on responsive design and modern UI components.",
+    url: img4
+  },
+  {
+    giturl: "#",
     name: "Flipkart Clone",
     role: "Frontend Developer",
     para: "Developed a Flipkart-inspired clone using React, Bootstrap, HTML, and CSS. Focused on component-based structure, responsive UI, and product layouts.",
     url: img6
+  },
+  {
+    giturl: "https://github.com/mitrakholiya/internet-Research",
+    name: "Internet Research",
+    role: "Frontend Developer",
+    para: "Designed a static responsive webpage using HTML and CSS to display research content with a clean and minimal layout.",
+    url: img1
+  },
+  {
+    giturl: "#",
+    name: "Amazon Clone",
+    role: "Frontend Developer",
+    para: "Built a clone of Amazon’s landing page using only HTML and CSS, focusing on layout, navigation bar, and responsive product sections.",
+    url: img2
+  },
+  {
+    giturl: "https://github.com/mitrakholiya/Ninom-Fruit-Template",
+    name: "Fruit Shop",
+    role: "Frontend Developer",
+    para: "Created an e-commerce template for a fruit shop using HTML, CSS, and Bootstrap. Implemented grid layouts and responsive components for better UX.",
+    url: img3
   },
 ];
