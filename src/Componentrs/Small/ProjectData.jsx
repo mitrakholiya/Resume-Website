5ace62eb29795cdbe78eaaa2160e9c2501d997cc
mit <meetrakholiya31@gmail.com>
2025-11-17 09:09:17 +0530

    Update

--- src/Componentrs/Small/ProjectData.jsx
+++ src/Componentrs/Small/ProjectData.jsx
@@ -10,52 +10,35 @@ export let webTemp = [
 
 
   {
-    giturl: "#",
+    giturl:"https://mitrakholiya.github.io/Portfolio-Website/",
+    giturl2: "https://github.com/mitrakholiya/Portfolio-Website",
     name: "Portfolio Website",
     role: "Frontend Developer",
     para: "Developed a Portfolio Website using React, Bootstrap, HTML, and CSS. Focused on component-based structure, responsive UI, and product layouts.",
     url: img7
   },
   {
-    giturl: "https://github.com/mitrakholiya/Ice-Cream-Web-site",
+    giturl:"https://mitrakholiya.github.io/Ice-Cream-Web-site/",
+    giturl2: "https://github.com/mitrakholiya/Ice-Cream-Web-site",
     name: "Ice-Cream Store",
     role: "Frontend Developer",
     para: "Built a fun and engaging ice-cream shop template using HTML, CSS, Bootstrap, and JavaScript. Added interactive elements for better user engagement.",
     url: img5
   },
   {
-    giturl: "https://github.com/mitrakholiya/Coffee-Website",
+    giturl:"https://mitrakholiya.github.io/Coffee-Website/",
+    giturl2: "https://github.com/mitrakholiya/Coffee-Website",
     name: "Coffee Shop",
     role: "Frontend Developer",
     para: "Developed a stylish coffee shop website using HTML, CSS, and Bootstrap. Focused on responsive design and modern UI components.",
     url: img4
   },
   {
-    giturl: "#",
+    giturl:"",
+    giturl2: "#",
     name: "Flipkart Clone",
     role: "Frontend Developer",
     para: "Developed a Flipkart-inspired clone using React, Bootstrap, HTML, and CSS. Focused on component-based structure, responsive UI, and product layouts.",
     url: img6
-  },
-  {
-    giturl: "https://github.com/mitrakholiya/internet-Research",
-    name: "Internet Research",
-    role: "Frontend Developer",
-    para: "Designed a static responsive webpage using HTML and CSS to display research content with a clean and minimal layout.",
-    url: img1
-  },
-  {
-    giturl: "#",
-    name: "Amazon Clone",
-    role: "Frontend Developer",
-    para: "Built a clone of Amazon’s landing page using only HTML and CSS, focusing on layout, navigation bar, and responsive product sections.",
-    url: img2
-  },
-  {
-    giturl: "https://github.com/mitrakholiya/Ninom-Fruit-Template",
-    name: "Fruit Shop",
-    role: "Frontend Developer",
-    para: "Created an e-commerce template for a fruit shop using HTML, CSS, and Bootstrap. Implemented grid layouts and responsive components for better UX.",
-    url: img3
   },
 ];
